ef243530bdf2d9d653c678e030e13ba2c72f6471
Gabriel Francois <gfrancoi@bess-f3r4s4.clusters.42paris.fr>
2025-01-13 19:57:00 +0100

    feat: add the new strbuilder

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -1,37 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 13:00:01 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/01/13 19:55:42 by gfrancoi         ###   ########.fr       */
+/*   Created: 2024/11/18 13:03:19 by gfrancoi          #+#    #+#             */
+/*   Updated: 2025/01/13 19:45:31 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+void	ft_putnbr_fd(int n, int fd)
 {
-	char	*sub;
-	size_t	sub_len;
-	size_t	i;
-
-	if (!s)
-		return (0);
-	sub_len = 0;
-	while (start < ft_strlen(s) && sub_len < len && s[start + sub_len])
-		sub_len++;
-	sub = ft_calloc(sub_len + 1, sizeof(char));
-	if (!sub)
-		return (0);
-	i = 0;
-	while (i < sub_len)
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
 	{
-		sub[i] = s[start + i];
-		i++;
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n > 9)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
 	}
-	sub[i] = 0;
-	return (sub);
 }
