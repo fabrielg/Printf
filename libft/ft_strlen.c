2e507160f12905dfe89fe84665b43e36d96030ed
fabrielg <gfrancois2003@gmail.com>
2025-01-19 14:20:52 +0100

    fix: protect ft_strlen, return 0 if s is NULL

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 18:03:23 by gfrancoi          #+#    #+#             */
-/*   Updated: 2024/11/18 15:59:00 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/01/19 14:15:04 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	while (s[i])
 		i++;
